b863d52503d384e72b2525b704559bc0d7278bec
noorong <100012522+noorong@users.noreply.github.com>
2023-01-27 23:54:25 +0900

    [level 0] Title: 소인수분해, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = [];
+    for(let i = 2; i <= n; i++) {
+        
+        if(n % i == 0) {
+        while(n % i == 0) n = n/i ;
+        answer.push(i)
+    }
+    }
+    return answer;
+}
