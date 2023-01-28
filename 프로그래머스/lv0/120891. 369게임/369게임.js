4500a1ef93d2cc6a998c57cbd86ed2e9fc161067
noorong <100012522+noorong@users.noreply.github.com>
2023-01-28 17:45:34 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,13 +1,4 @@
-function solution(order) {  
-    var answer = 0;
-    let arr = String(order).split("");
-                
-    for(let i = 0; i < arr.length; i++) {
-    // console.log(typeof arr[i], arr[i])
-        if (arr[i] == "3"||arr[i] == "6"||arr[i] == "9") {
-            answer++ 
-        }
-    }
-                
-    return answer;
+function solution(order) { 
+    return String(order).split('').filter(v => v == 3 || v == 6 || v == 9).length;
 }
+
