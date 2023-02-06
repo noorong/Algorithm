37a2b900f64ebfbf6c85a025f3c8d7277eeaebae
noorong <100012522+noorong@users.noreply.github.com>
2023-02-06 22:58:10 +0900

    [level 0] Title: 로그인 성공?, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,16 +1,11 @@
 function solution(id_pw, db) {
     var  answer="";
-    // for(let i=0; i<db.length;i++){
-    //     db[i][0]===id_pw[0] && db[i][1] === id_pw[1] ? 
-    //         answer= "login" : 
-    //     db[i][0]===id_pw[0] && db[i][1] !== id_pw[1] ? 
-    //         answer= "wrong pw" : 
-    //     answer= "fail"
-    // }
-    db.map((arr, i)=>{
-        if(arr[0]===id_pw[0]&&arr[1]===id_pw[1]) answer= "login"
-        else if(arr[0]===id_pw[0] && arr[1] !== id_pw[1]) answer= "wrong pw" 
-        else if(arr[0]!==id_pw[0] && arr[1] !== id_pw[1]) answer= "fail"
+    
+    db.map(arr=>{
+        if(arr[0]==id_pw[0]&&arr[1]==id_pw[1]) answer= "login"
+        else if(arr[0]==id_pw[0] && arr[1] != id_pw[1]) answer= "wrong pw" 
+        else if(arr[0]!=id_pw[0] && arr[1] != id_pw[1]) answer= "fail"
     })
+    
     return answer;
 }
