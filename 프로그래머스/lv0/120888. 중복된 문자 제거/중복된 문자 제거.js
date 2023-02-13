02823ee3de204c5fab2f1b962ba28fcf549a4266
noorong <100012522+noorong@users.noreply.github.com>
2023-02-13 16:34:22 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -0,0 +1,12 @@
+function solution(my_string) {
+    let arr = my_string.split('');
+    let newArr = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        if(!newArr.includes(arr[i])) newArr.push(arr[i])
+    }
+    
+    
+    return newArr.join('');
+    
+}
