8381d0c0935cd25510d7f9e66b5658045a3ca8ff
noorong <100012522+noorong@users.noreply.github.com>
2023-02-23 00:00:12 +0900

    [level 2] Title: 피보나치 수, Time: 4.32 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    var answer = [0,1]
+    
+    for (let i = 2; i <= n ; i++) {
+        (answer[i]) = (answer[i-1] + answer[i-2]) % 1234567
+    }
+    
+    return answer[n];
+}
