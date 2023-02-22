44c524de30514baf021bfffa4743bcbd376adabe
noorong <100012522+noorong@users.noreply.github.com>
2023-02-22 23:15:57 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.04 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,5 +1,5 @@
 function solution(arr) {
-    var answer = arr.indexOf(Math.min(...arr));
-    let a = arr.splice(answer,1)
+    var min = arr.indexOf(Math.min(...arr));
+    let answer = arr.splice(min,1)
     return arr.length > 1 ? arr : [-1];
 }
