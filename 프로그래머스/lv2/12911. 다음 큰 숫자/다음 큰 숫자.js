4b91d5e03aea870c140be77c6fd473141453f1af
noorong <100012522+noorong@users.noreply.github.com>
2023-02-23 14:29:17 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = n.toString(2).split('').filter(v => v == 1);
+    
+    n=n+1
+    for(n; n < n+n; n++) {
+        let arr = (n).toString(2).split('').filter(v => v == 1);
+        if (arr.length == answer.length) break ;
+    }
+    
+    return n;
+}
