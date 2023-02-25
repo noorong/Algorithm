10ffcab416aeee2fac290aa8ceb7e5e8a661f997
noorong <100012522+noorong@users.noreply.github.com>
2023-02-26 01:38:21 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,16 +1,28 @@
-function solution(brown, yellow) {
-    let arr = []
-    if(Number.isInteger(Math.sqrt(yellow))) return [Math.sqrt(yellow)+2,Math.sqrt(yellow)+2]
-    else {
-    for (let i = 1; i <= yellow; i++) yellow % i === 0 && arr.push(i);
-    let tmp = [arr[arr.length/2]+2 , arr[arr.length/2 - 1]+2];
+// function solution(brown, yellow) {
+//     let arr = []
+//     if(Number.isInteger(Math.sqrt(yellow))) return [Math.sqrt(yellow)+2,Math.sqrt(yellow)+2]
+//     else {
+//     for (let i = 1; i <= yellow; i++) yellow % i === 0 && arr.push(i);
+//     let tmp = [arr[arr.length/2]+2 , arr[arr.length/2 - 1]+2];
         
-    if (tmp[0] * tmp[1] - yellow == brown) return tmp;
+//     if (tmp[0] * tmp[1] - yellow == brown) return tmp;
     
-    for (let j = 1; j <= arr.length/2; j++) {
-        tmp = [arr[arr.length/2 + j]+2 , arr[arr.length/2 - 1 - j]+2]
-        if (tmp[0] * tmp[1] - yellow == brown) return tmp
-    }
+//     for (let j = 1; j <= arr.length/2; j++) {
+//         tmp = [arr[arr.length/2 + j]+2 , arr[arr.length/2 - 1 - j]+2]
+//         if (tmp[0] * tmp[1] - yellow == brown) return tmp
+//     }
         
+//     }
+// }
+
+function solution(brown, red) {
+    var answer = [];
+    for (var i = 3; i <= (brown+red)/i; i++) {
+        var x = Math.floor((brown+red)/i);
+        if( (x-2)*(i-2)=== red) {
+            break;
+        }
     }
+ 
+    return [x,i];  // 가로 x, 세로 i
 }
