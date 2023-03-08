0a23a487875de4e81661d9f33d9d674fc4c83098
noorong <100012522+noorong@users.noreply.github.com>
2023-03-08 16:40:18 +0900

    [level 0] Title: 등수 매기기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -1,13 +1,10 @@
 function solution(score) {
     var answer = [];
-    let avg = [];
     let rank = [];
     
     for (let x of score) answer.push(x.reduce((a,b) => (a+b) / 2))
     for (let i = 0; i < answer.length; i++) {
-        avg = answer.slice().sort((a,b) => b - a);
-        rank[i] = avg.indexOf(answer[i]) + 1
+        rank[i] = answer.slice().sort((a,b) => b - a).indexOf(answer[i]) + 1;
     }
-    console.log(avg)
     return rank;
 }
